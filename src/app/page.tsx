7f7576757d3820bb4f76bb3cd9ee2014a3d79056
Dakotah Lyons <44170861+Dakotah312@users.noreply.github.com>
2025-07-21 22:39:11 -0500

    linkedin and skills app router page (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl font-bold text-center sm:text-left">
-          🚧 Under Construction 🚧
-        </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
@@ -23,6 +20,21 @@ export default function Home() {
             height={20}
           />
           Github
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://www.linkedin.com/in/dakotah-lyons/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/images/InBug-White.png"
+            alt="Linkedin icon"
+            width={20}
+            height={20}
+          />
+          Linkedin
         </a>
       </footer>
     </div>
